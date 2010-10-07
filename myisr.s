31d8240bbaba0abe9a25cd526d8318b28834e05f
Peter Girard Hamilton <hamiltop@lavender.ee.byu.edu>
2010-10-06 21:57:27 -0600

    all files in place

---
+++ myisr.s
@@ -0,0 +1,78 @@
+resetRoutine:
+	push ax
+	push bx
+	push cx
+	push dx
+	push si
+	push di
+	push bp
+	push es
+	push ds
+	sti
+	call reset
+	cli
+	mov	al, 0x20
+	out	0x20, al
+	pop ds
+	pop es
+	pop bp
+	pop di
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	iret
+	
+tickRoutine:
+	push ax
+	push bx
+	push cx
+	push dx
+	push si
+	push di
+	push bp
+	push es
+	push ds
+	sti
+	call tick
+	cli
+	mov	al, 0x20
+	out	0x20, al
+	pop ds
+	pop es
+	pop bp
+	pop di
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	iret
+
+			
+	keyRoutine:
+	push ax
+	push bx
+	push cx
+	push dx
+	push si
+	push di
+	push bp
+	push es
+	push ds
+	sti
+	call key
+	cli
+	mov	al, 0x20
+	out	0x20, al
+	pop ds
+	pop es
+	pop bp
+	pop di
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	iret
